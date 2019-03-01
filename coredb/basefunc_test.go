7eb39c85d71ea11cd22eb6c1630819e96f9406d5
zhs007 <sssxueren@gmail.com>
2019-03-01 14:05:13 +0800

    - add unit test for coredb

--- coredb/basefunc_test.go
+++ coredb/basefunc_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/graphql-go/graphql"
 	"github.com/zhs007/ankadb"
+	"github.com/zhs007/jarviscore/coredb/proto"
 	"github.com/zhs007/jarviscore/crypto"
 )
 
@@ -133,6 +134,628 @@ func TestBaseFunc(t *testing.T) {
 		t.Fatalf("TestBaseFunc updPrivateData return StrPubKey fail! (%v - %v)", pd.StrPubKey, jarviscrypto.Base58Encode(privKey.ToPublicBytes()))
 
 		return
+	}
+
+	//------------------------------------------------------------------------
+	// updNodeInfo test001
+
+	test001PriKey := jarviscrypto.GenerateKey()
+
+	cni1 := &coredbpb.NodeInfo{
+		ServAddr:        "127.0.0.1",
+		Addr:            test001PriKey.ToAddress(),
+		Name:            "test001",
+		ConnectNums:     0,
+		ConnectedNums:   0,
+		CtrlID:          0,
+		LstClientAddr:   nil,
+		AddTime:         time.Now().Unix(),
+		ConnectMe:       false,
+		ConnectNode:     false,
+		NodeTypeVersion: "0.1.0",
+		NodeType:        "normal",
+		CoreVersion:     "0.1.0",
+		LastRecvMsgID:   1,
+	}
+
+	cn1, err := updNodeInfo(ankaDB, cni1)
+	if err != nil {
+		t.Fatalf("TestBaseFunc updNodeInfo %v", err)
+
+		return
+	}
+
+	if cni1.ServAddr != cn1.ServAddr {
+		t.Fatalf("TestBaseFunc updNodeInfo return ServAddr fail! (%v - %v)", cni1.ServAddr, cn1.ServAddr)
+
+		return
+	}
+
+	if cni1.Addr != cn1.Addr {
+		t.Fatalf("TestBaseFunc updNodeInfo return Addr fail! (%v - %v)", cni1.Addr, cn1.Addr)
+
+		return
+	}
+
+	if cni1.Name != cn1.Name {
+		t.Fatalf("TestBaseFunc updNodeInfo return Name fail! (%v - %v)", cni1.Name, cn1.Name)
+
+		return
+	}
+
+	if cni1.ConnectNums != cn1.ConnectNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectNums fail! (%v - %v)", cni1.ConnectNums, cn1.ConnectNums)
+
+		return
+	}
+
+	if cni1.ConnectedNums != cn1.ConnectedNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+		return
+	}
+
+	if cni1.CtrlID != cn1.CtrlID {
+		t.Fatalf("TestBaseFunc updNodeInfo return CtrlID fail! (%v - %v)", cni1.CtrlID, cn1.CtrlID)
+
+		return
+	}
+
+	if cni1.ConnectedNums != cn1.ConnectedNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+		return
+	}
+
+	if cni1.AddTime != cn1.AddTime {
+		t.Fatalf("TestBaseFunc updNodeInfo return AddTime fail! (%v - %v)", cni1.AddTime, cn1.AddTime)
+
+		return
+	}
+
+	if cni1.ConnectMe != cn1.ConnectMe {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectMe fail! (%v - %v)", cni1.ConnectMe, cn1.ConnectMe)
+
+		return
+	}
+
+	if cni1.ConnectNode != cn1.ConnectNode {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectNode fail! (%v - %v)", cni1.ConnectNode, cn1.ConnectNode)
+
+		return
+	}
+
+	if cni1.NodeTypeVersion != cn1.NodeTypeVersion {
+		t.Fatalf("TestBaseFunc updNodeInfo return NodeTypeVersion fail! (%v - %v)", cni1.NodeTypeVersion, cn1.NodeTypeVersion)
+
+		return
+	}
+
+	if cni1.NodeType != cn1.NodeType {
+		t.Fatalf("TestBaseFunc updNodeInfo return NodeType fail! (%v - %v)", cni1.NodeType, cn1.NodeType)
+
+		return
+	}
+
+	if cni1.CoreVersion != cn1.CoreVersion {
+		t.Fatalf("TestBaseFunc updNodeInfo return CoreVersion fail! (%v - %v)", cni1.CoreVersion, cn1.CoreVersion)
+
+		return
+	}
+
+	if cni1.LastRecvMsgID != cn1.LastRecvMsgID {
+		t.Fatalf("TestBaseFunc updNodeInfo return LastRecvMsgID fail! (%v - %v)", cni1.LastRecvMsgID, cn1.LastRecvMsgID)
+
+		return
+	}
+
+	//------------------------------------------------------------------------
+	// updNodeInfo test002
+
+	test002PriKey := jarviscrypto.GenerateKey()
+
+	cni2 := &coredbpb.NodeInfo{
+		ServAddr:        "127.0.0.1",
+		Addr:            test002PriKey.ToAddress(),
+		Name:            "test002",
+		ConnectNums:     0,
+		ConnectedNums:   0,
+		CtrlID:          0,
+		LstClientAddr:   nil,
+		AddTime:         time.Now().Unix(),
+		ConnectMe:       false,
+		ConnectNode:     false,
+		NodeTypeVersion: "0.1.0",
+		NodeType:        "normal",
+		CoreVersion:     "0.1.0",
+		LastRecvMsgID:   1,
+	}
+
+	cn2, err := updNodeInfo(ankaDB, cni2)
+	if err != nil {
+		t.Fatalf("TestBaseFunc updNodeInfo %v", err)
+
+		return
+	}
+
+	if cni2.ServAddr != cn2.ServAddr {
+		t.Fatalf("TestBaseFunc updNodeInfo return ServAddr fail! (%v - %v)", cni2.ServAddr, cn2.ServAddr)
+
+		return
+	}
+
+	if cni2.Addr != cn2.Addr {
+		t.Fatalf("TestBaseFunc updNodeInfo return Addr fail! (%v - %v)", cni2.Addr, cn2.Addr)
+
+		return
+	}
+
+	if cni2.Name != cn2.Name {
+		t.Fatalf("TestBaseFunc updNodeInfo return Name fail! (%v - %v)", cni2.Name, cn2.Name)
+
+		return
+	}
+
+	if cni2.ConnectNums != cn2.ConnectNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectNums fail! (%v - %v)", cni2.ConnectNums, cn2.ConnectNums)
+
+		return
+	}
+
+	if cni2.ConnectedNums != cn2.ConnectedNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+		return
+	}
+
+	if cni2.CtrlID != cn2.CtrlID {
+		t.Fatalf("TestBaseFunc updNodeInfo return CtrlID fail! (%v - %v)", cni2.CtrlID, cn2.CtrlID)
+
+		return
+	}
+
+	if cni2.ConnectedNums != cn2.ConnectedNums {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+		return
+	}
+
+	if cni2.AddTime != cn2.AddTime {
+		t.Fatalf("TestBaseFunc updNodeInfo return AddTime fail! (%v - %v)", cni2.AddTime, cn2.AddTime)
+
+		return
+	}
+
+	if cni2.ConnectMe != cn2.ConnectMe {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectMe fail! (%v - %v)", cni2.ConnectMe, cn2.ConnectMe)
+
+		return
+	}
+
+	if cni2.ConnectNode != cn2.ConnectNode {
+		t.Fatalf("TestBaseFunc updNodeInfo return ConnectNode fail! (%v - %v)", cni2.ConnectNode, cn2.ConnectNode)
+
+		return
+	}
+
+	if cni2.NodeTypeVersion != cn2.NodeTypeVersion {
+		t.Fatalf("TestBaseFunc updNodeInfo return NodeTypeVersion fail! (%v - %v)", cni2.NodeTypeVersion, cn2.NodeTypeVersion)
+
+		return
+	}
+
+	if cni2.NodeType != cn2.NodeType {
+		t.Fatalf("TestBaseFunc updNodeInfo return NodeType fail! (%v - %v)", cni2.NodeType, cn2.NodeType)
+
+		return
+	}
+
+	if cni2.CoreVersion != cn2.CoreVersion {
+		t.Fatalf("TestBaseFunc updNodeInfo return CoreVersion fail! (%v - %v)", cni2.CoreVersion, cn2.CoreVersion)
+
+		return
+	}
+
+	if cni2.LastRecvMsgID != cn2.LastRecvMsgID {
+		t.Fatalf("TestBaseFunc updNodeInfo return LastRecvMsgID fail! (%v - %v)", cni2.LastRecvMsgID, cn2.LastRecvMsgID)
+
+		return
+	}
+
+	//------------------------------------------------------------------------
+	// trustNode
+
+	pd, err = trustNode(ankaDB, test001PriKey.ToAddress())
+	if err != nil {
+		t.Fatalf("TestBaseFunc trustNode %v", err)
+
+		return
+	}
+
+	if pd.PriKey != nil || pd.PubKey != nil || pd.StrPriKey != "" {
+		t.Fatalf("TestBaseFunc trustNode return PriKey or PubKey or StrPriKey is non-nil")
+
+		return
+	}
+
+	if pd.StrPubKey != jarviscrypto.Base58Encode(privKey.ToPublicBytes()) {
+		t.Fatalf("TestBaseFunc trustNode return StrPubKey fail! (%v - %v)", pd.StrPubKey, jarviscrypto.Base58Encode(privKey.ToPublicBytes()))
+
+		return
+	}
+
+	//------------------------------------------------------------------------
+	// getNodeInfo test001
+
+	cn1, err = getNodeInfo(ankaDB, test001PriKey.ToAddress())
+	if err != nil {
+		t.Fatalf("TestBaseFunc getNodeInfo %v", err)
+
+		return
+	}
+
+	if cni1.ServAddr != cn1.ServAddr {
+		t.Fatalf("TestBaseFunc getNodeInfo return ServAddr fail! (%v - %v)", cni1.ServAddr, cn1.ServAddr)
+
+		return
+	}
+
+	if cni1.Addr != cn1.Addr {
+		t.Fatalf("TestBaseFunc getNodeInfo return Addr fail! (%v - %v)", cni1.Addr, cn1.Addr)
+
+		return
+	}
+
+	if cni1.Name != cn1.Name {
+		t.Fatalf("TestBaseFunc getNodeInfo return Name fail! (%v - %v)", cni1.Name, cn1.Name)
+
+		return
+	}
+
+	if cni1.ConnectNums != cn1.ConnectNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectNums fail! (%v - %v)", cni1.ConnectNums, cn1.ConnectNums)
+
+		return
+	}
+
+	if cni1.ConnectedNums != cn1.ConnectedNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+		return
+	}
+
+	if cni1.CtrlID != cn1.CtrlID {
+		t.Fatalf("TestBaseFunc getNodeInfo return CtrlID fail! (%v - %v)", cni1.CtrlID, cn1.CtrlID)
+
+		return
+	}
+
+	if cni1.ConnectedNums != cn1.ConnectedNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+		return
+	}
+
+	if cni1.AddTime != cn1.AddTime {
+		t.Fatalf("TestBaseFunc getNodeInfo return AddTime fail! (%v - %v)", cni1.AddTime, cn1.AddTime)
+
+		return
+	}
+
+	if cni1.ConnectMe != cn1.ConnectMe {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectMe fail! (%v - %v)", cni1.ConnectMe, cn1.ConnectMe)
+
+		return
+	}
+
+	if cni1.ConnectNode != cn1.ConnectNode {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectNode fail! (%v - %v)", cni1.ConnectNode, cn1.ConnectNode)
+
+		return
+	}
+
+	if cni1.NodeTypeVersion != cn1.NodeTypeVersion {
+		t.Fatalf("TestBaseFunc getNodeInfo return NodeTypeVersion fail! (%v - %v)", cni1.NodeTypeVersion, cn1.NodeTypeVersion)
+
+		return
+	}
+
+	if cni1.NodeType != cn1.NodeType {
+		t.Fatalf("TestBaseFunc getNodeInfo return NodeType fail! (%v - %v)", cni1.NodeType, cn1.NodeType)
+
+		return
+	}
+
+	if cni1.CoreVersion != cn1.CoreVersion {
+		t.Fatalf("TestBaseFunc getNodeInfo return CoreVersion fail! (%v - %v)", cni1.CoreVersion, cn1.CoreVersion)
+
+		return
+	}
+
+	if cni1.LastRecvMsgID != cn1.LastRecvMsgID {
+		t.Fatalf("TestBaseFunc getNodeInfo return LastRecvMsgID fail! (%v - %v)", cni1.LastRecvMsgID, cn1.LastRecvMsgID)
+
+		return
+	}
+
+	//------------------------------------------------------------------------
+	// getNodeInfo test002
+
+	cn2, err = getNodeInfo(ankaDB, test002PriKey.ToAddress())
+	if err != nil {
+		t.Fatalf("TestBaseFunc getNodeInfo %v", err)
+
+		return
+	}
+
+	if cni2.ServAddr != cn2.ServAddr {
+		t.Fatalf("TestBaseFunc getNodeInfo return ServAddr fail! (%v - %v)", cni2.ServAddr, cn2.ServAddr)
+
+		return
+	}
+
+	if cni2.Addr != cn2.Addr {
+		t.Fatalf("TestBaseFunc getNodeInfo return Addr fail! (%v - %v)", cni2.Addr, cn2.Addr)
+
+		return
+	}
+
+	if cni2.Name != cn2.Name {
+		t.Fatalf("TestBaseFunc getNodeInfo return Name fail! (%v - %v)", cni2.Name, cn2.Name)
+
+		return
+	}
+
+	if cni2.ConnectNums != cn2.ConnectNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectNums fail! (%v - %v)", cni2.ConnectNums, cn2.ConnectNums)
+
+		return
+	}
+
+	if cni2.ConnectedNums != cn2.ConnectedNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+		return
+	}
+
+	if cni2.CtrlID != cn2.CtrlID {
+		t.Fatalf("TestBaseFunc getNodeInfo return CtrlID fail! (%v - %v)", cni2.CtrlID, cn2.CtrlID)
+
+		return
+	}
+
+	if cni2.ConnectedNums != cn2.ConnectedNums {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+		return
+	}
+
+	if cni2.AddTime != cn2.AddTime {
+		t.Fatalf("TestBaseFunc getNodeInfo return AddTime fail! (%v - %v)", cni2.AddTime, cn2.AddTime)
+
+		return
+	}
+
+	if cni2.ConnectMe != cn2.ConnectMe {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectMe fail! (%v - %v)", cni2.ConnectMe, cn2.ConnectMe)
+
+		return
+	}
+
+	if cni2.ConnectNode != cn2.ConnectNode {
+		t.Fatalf("TestBaseFunc getNodeInfo return ConnectNode fail! (%v - %v)", cni2.ConnectNode, cn2.ConnectNode)
+
+		return
+	}
+
+	if cni2.NodeTypeVersion != cn2.NodeTypeVersion {
+		t.Fatalf("TestBaseFunc getNodeInfo return NodeTypeVersion fail! (%v - %v)", cni2.NodeTypeVersion, cn2.NodeTypeVersion)
+
+		return
+	}
+
+	if cni2.NodeType != cn2.NodeType {
+		t.Fatalf("TestBaseFunc getNodeInfo return NodeType fail! (%v - %v)", cni2.NodeType, cn2.NodeType)
+
+		return
+	}
+
+	if cni2.CoreVersion != cn2.CoreVersion {
+		t.Fatalf("TestBaseFunc getNodeInfo return CoreVersion fail! (%v - %v)", cni2.CoreVersion, cn2.CoreVersion)
+
+		return
+	}
+
+	if cni2.LastRecvMsgID != cn2.LastRecvMsgID {
+		t.Fatalf("TestBaseFunc getNodeInfo return LastRecvMsgID fail! (%v - %v)", cni2.LastRecvMsgID, cn2.LastRecvMsgID)
+
+		return
+	}
+
+	//------------------------------------------------------------------------
+	// getNodeInfos test002
+
+	lstnode, err := getNodeInfos(ankaDB, -1, 0, 100)
+	if err != nil {
+		t.Fatalf("TestBaseFunc getNodeInfos %v", err)
+
+		return
+	}
+
+	for i := 0; i < len(lstnode.Nodes); i++ {
+		if lstnode.Nodes[i].Addr == cni1.Addr {
+			cn1 = lstnode.Nodes[i]
+
+			if cni1.ServAddr != cn1.ServAddr {
+				t.Fatalf("TestBaseFunc getNodeInfos return ServAddr fail! (%v - %v)", cni1.ServAddr, cn1.ServAddr)
+
+				return
+			}
+
+			if cni1.Addr != cn1.Addr {
+				t.Fatalf("TestBaseFunc getNodeInfos return Addr fail! (%v - %v)", cni1.Addr, cn1.Addr)
+
+				return
+			}
+
+			if cni1.Name != cn1.Name {
+				t.Fatalf("TestBaseFunc getNodeInfos return Name fail! (%v - %v)", cni1.Name, cn1.Name)
+
+				return
+			}
+
+			if cni1.ConnectNums != cn1.ConnectNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectNums fail! (%v - %v)", cni1.ConnectNums, cn1.ConnectNums)
+
+				return
+			}
+
+			if cni1.ConnectedNums != cn1.ConnectedNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+				return
+			}
+
+			if cni1.CtrlID != cn1.CtrlID {
+				t.Fatalf("TestBaseFunc getNodeInfos return CtrlID fail! (%v - %v)", cni1.CtrlID, cn1.CtrlID)
+
+				return
+			}
+
+			if cni1.ConnectedNums != cn1.ConnectedNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectedNums fail! (%v - %v)", cni1.ConnectedNums, cn1.ConnectedNums)
+
+				return
+			}
+
+			if cni1.AddTime != cn1.AddTime {
+				t.Fatalf("TestBaseFunc getNodeInfos return AddTime fail! (%v - %v)", cni1.AddTime, cn1.AddTime)
+
+				return
+			}
+
+			if cni1.ConnectMe != cn1.ConnectMe {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectMe fail! (%v - %v)", cni1.ConnectMe, cn1.ConnectMe)
+
+				return
+			}
+
+			if cni1.ConnectNode != cn1.ConnectNode {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectNode fail! (%v - %v)", cni1.ConnectNode, cn1.ConnectNode)
+
+				return
+			}
+
+			if cni1.NodeTypeVersion != cn1.NodeTypeVersion {
+				t.Fatalf("TestBaseFunc getNodeInfos return NodeTypeVersion fail! (%v - %v)", cni1.NodeTypeVersion, cn1.NodeTypeVersion)
+
+				return
+			}
+
+			if cni1.NodeType != cn1.NodeType {
+				t.Fatalf("TestBaseFunc getNodeInfos return NodeType fail! (%v - %v)", cni1.NodeType, cn1.NodeType)
+
+				return
+			}
+
+			if cni1.CoreVersion != cn1.CoreVersion {
+				t.Fatalf("TestBaseFunc getNodeInfos return CoreVersion fail! (%v - %v)", cni1.CoreVersion, cn1.CoreVersion)
+
+				return
+			}
+
+			if cni1.LastRecvMsgID != cn1.LastRecvMsgID {
+				t.Fatalf("TestBaseFunc getNodeInfos return LastRecvMsgID fail! (%v - %v)", cni1.LastRecvMsgID, cn1.LastRecvMsgID)
+
+				return
+			}
+		} else if lstnode.Nodes[i].Addr == cni2.Addr {
+			cn2 = lstnode.Nodes[i]
+
+			if cni2.ServAddr != cn2.ServAddr {
+				t.Fatalf("TestBaseFunc getNodeInfos return ServAddr fail! (%v - %v)", cni2.ServAddr, cn2.ServAddr)
+
+				return
+			}
+
+			if cni2.Addr != cn2.Addr {
+				t.Fatalf("TestBaseFunc getNodeInfos return Addr fail! (%v - %v)", cni2.Addr, cn2.Addr)
+
+				return
+			}
+
+			if cni2.Name != cn2.Name {
+				t.Fatalf("TestBaseFunc getNodeInfos return Name fail! (%v - %v)", cni2.Name, cn2.Name)
+
+				return
+			}
+
+			if cni2.ConnectNums != cn2.ConnectNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectNums fail! (%v - %v)", cni2.ConnectNums, cn2.ConnectNums)
+
+				return
+			}
+
+			if cni2.ConnectedNums != cn2.ConnectedNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+				return
+			}
+
+			if cni2.CtrlID != cn2.CtrlID {
+				t.Fatalf("TestBaseFunc getNodeInfos return CtrlID fail! (%v - %v)", cni2.CtrlID, cn2.CtrlID)
+
+				return
+			}
+
+			if cni2.ConnectedNums != cn2.ConnectedNums {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectedNums fail! (%v - %v)", cni2.ConnectedNums, cn2.ConnectedNums)
+
+				return
+			}
+
+			if cni2.AddTime != cn2.AddTime {
+				t.Fatalf("TestBaseFunc getNodeInfos return AddTime fail! (%v - %v)", cni2.AddTime, cn2.AddTime)
+
+				return
+			}
+
+			if cni2.ConnectMe != cn2.ConnectMe {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectMe fail! (%v - %v)", cni2.ConnectMe, cn2.ConnectMe)
+
+				return
+			}
+
+			if cni2.ConnectNode != cn2.ConnectNode {
+				t.Fatalf("TestBaseFunc getNodeInfos return ConnectNode fail! (%v - %v)", cni2.ConnectNode, cn2.ConnectNode)
+
+				return
+			}
+
+			if cni2.NodeTypeVersion != cn2.NodeTypeVersion {
+				t.Fatalf("TestBaseFunc getNodeInfos return NodeTypeVersion fail! (%v - %v)", cni2.NodeTypeVersion, cn2.NodeTypeVersion)
+
+				return
+			}
+
+			if cni2.NodeType != cn2.NodeType {
+				t.Fatalf("TestBaseFunc getNodeInfos return NodeType fail! (%v - %v)", cni2.NodeType, cn2.NodeType)
+
+				return
+			}
+
+			if cni2.CoreVersion != cn2.CoreVersion {
+				t.Fatalf("TestBaseFunc getNodeInfos return CoreVersion fail! (%v - %v)", cni2.CoreVersion, cn2.CoreVersion)
+
+				return
+			}
+
+			if cni2.LastRecvMsgID != cn2.LastRecvMsgID {
+				t.Fatalf("TestBaseFunc getNodeInfos return LastRecvMsgID fail! (%v - %v)", cni2.LastRecvMsgID, cn2.LastRecvMsgID)
+
+				return
+			}
+		}
 	}
 
 	t.Logf("TestBaseFunc is OK")
