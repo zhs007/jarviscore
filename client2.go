f15a5f842dab2ebcebe3e9e5a51408dabdb83b7e
zhs007 <sssxueren@gmail.com>
2019-03-09 11:39:12 +0800

    - fixed bug for warning

--- client2.go
+++ client2.go
@@ -72,7 +72,9 @@ func (task *clientTask) Run(ctx context.Context) error {
 			task.client.node.mgrEvent.onNodeEvent(ctx, EventOnIConnectNodeFail, task.node)
 		}
 
-		jarvisbase.Warn("clientTask.Run:_connectNode", zap.Error(err))
+		if err != nil {
+			jarvisbase.Warn("clientTask.Run:_connectNode", zap.Error(err))
+		}
 
 		return err
 	}
