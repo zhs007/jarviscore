3d0db28f043c6cd9788fd961aeab6caa8c11ff34
zhs007 <sssxueren@gmail.com>
2019-05-25 16:38:49 +0800

    - updated warn logs

--- basedef/basedef.go
+++ basedef/basedef.go
@@ -1,7 +1,7 @@
 package basedef
 
 // VERSION - jarviscore version
-const VERSION = "0.7.182"
+const VERSION = "0.7.183"
 
 // TimeFailedServAddr - if the servaddr is failed, we won't try to connect it within this time
 const TimeFailedServAddr = 60
